a83d3c444dee9e7ef267a7b3b47533a6def99fd2
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-04 13:31:24 -0300

    Create terratest.go

---
+++ terraform/test/terratest.go
@@ -0,0 +1,26 @@
+
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../",
+	}
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	//output := terraform.Output(t, terraformOptions, "resource")
+	//assert.Equal(t, "resource", output)
+}
