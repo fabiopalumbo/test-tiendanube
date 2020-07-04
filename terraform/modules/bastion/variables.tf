2f88023fb4c62d59a3e808e2e6ae8ffe9a0ba60a
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-07-04 16:00:31 -0300

    Create variables.tf

---
+++ terraform/modules/bastion/variables.tf
@@ -0,0 +1,15 @@
+variable "public_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "aws_region" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "aws_amis" {}
+variable "sg_id" {}
+variable "vpc_name" {}
+variable "vpc_prefix" {}
+variable "project" {}
+variable "service_name" {}
+variable "aws_instance_type" {}
+variable "ami_id" {}
+variable "run_list" {}
